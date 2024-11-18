cde21788d971dc508c374f012875802bd9c91c3d
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-11-18 09:56:41 -0500

    comment out shows

--- test_gfisher_matrix.py
+++ test_gfisher_matrix.py
@@ -12,24 +12,24 @@ mt = mt.annotate_cols(phenotype=hl.agg.sum(mt.GT.n_alt_alleles()) - 20 + hl.rand
 mt = mt.select_rows(mt.gene, mt.weight)
 mt = mt.select_cols(mt.phenotype)
 mt = mt.select_globals()
-mt.describe()
+# mt.describe()
 
 mt = mt.add_row_index()
-mt.describe()
+# mt.describe()
 
 pval_ht = hl.linear_regression_rows(y=mt.phenotype, x=mt.X, covariates=[1.0])
-pval_ht.show()
+# pval_ht.show()
 
 row_cor = hl.row_correlation(mt.X)
 
 rce = row_cor.to_table_row_major()
 rce = rce.annotate(entries=hl.nd.array(rce.entries))
-rce.show()
+# rce.show()
 
 mt = mt.annotate_rows(row_cor=rce[mt.row_idx].entries, pval=pval_ht[mt.row_key].p_value)
-mt.rows().show()
+# mt.rows().show()
 
 mt = mt.annotate_rows(df=hl.literal(list(range(1, 1 + mt.count_rows())))[hl.int32(mt.row_idx)])
-mt.rows().show()
+# mt.rows().show()
 
 hl.gfisher_thing(mt.gene, mt.pval, mt.df, mt.weight, mt.row_cor, mt.row_idx)
