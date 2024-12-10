35cfaa66dcf620d477715478cab0089d66c31916
peter-howell <44275653+peter-howell@users.noreply.github.com>
2024-12-10 10:03:12 -0500

    add function for E_gX_p

--- hail/src/main/scala/is/hail/methods/gfisher/GFisherWeights.scala
+++ hail/src/main/scala/is/hail/methods/gfisher/GFisherWeights.scala
@@ -1,5 +1,7 @@
 package is.hail.methods.gfisher
 
+import is.hail.GaussKronrod
+
 import breeze.linalg.{DenseVector => BDV, DenseMatrix => BDM, _}
 
 object GFisherWeights {
@@ -16,6 +18,19 @@ object GFisherWeights {
     return hermite_scalar(x-mu, deg)
   }
 
+
+  /**
+    * Calculate E[g(X)^p] where X~N(mu,sigma)
+    *
+    * @param g function
+    * @param mu mean
+    * @param p power
+    * @param sigma std
+    */
+  def E_gX_p(g: Double => Double, mu: Double, p: Double, sigma:Double=1.0): Double = {
+    val integrator = new GaussKronrod(1e-9, 100)
+    return integrator.integrate((x) => math.pow(g(x), p) * Normal.density((x-mu)/sigma, 0, 1.0, false), mu-8*sigma, mu+8*sigma).estimate / sigma
+  }
 
 
 }
