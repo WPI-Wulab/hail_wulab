9c94492b81781b185a8c48fdf1c59755b5c3e236
peter-howell <44275653+peter-howell@users.noreply.github.com>
2025-01-17 16:17:05 -0500

    new functions

--- hail/src/main/scala/is/hail/methods/gfisher/GFisherWeights.scala
+++ hail/src/main/scala/is/hail/methods/gfisher/GFisherWeights.scala
@@ -77,6 +77,59 @@ object GFisherWeights {
 
     return M_out
   }
+
+
+  /**
+    * Calculate the covariance matrix Cov[T_i, T_j], where T_i=g(Z_i), T_j=g(Z_j),
+    * Z_i = Z_0i + mu_i*C_i, C_i ~ Bern(pi_i), Z_j = Z_0j + mu_j*C_j, C_j ~ Bern(pi_j),
+    * Cov[Z_0i, Z_0i] = M_ij,
+    *
+    * @param g transformation function
+    * @param mu mean vector
+    * @param pi probability vector
+    * @param M a positive-definite correlation matrix
+    * @param ORD a sequence of orders of Hermite polynomials (unused, as the original code does not use this)
+    */
+  def covMT_mix(
+    g: (Double) => Double,
+    mu: BDV[Double],
+    pi: BDV[Double],
+    M: BDM[Double],
+    ORD: Seq[Int]= Seq(1,2,3,4,5,6,7,8)
+  ): BDM[Double] = {
+    val pi2: BDM[Double] = pi * pi.t
+    val q: BDV[Double] = 1.0 - pi
+    val pq: BDM[Double] = pi * q.t
+    val qq: BDM[Double] = q * q.t
+    val covM: BDM[Double] = covM_gXgY(g, mu, mu, M, (0 to 8).toList)
+    val covM0: BDM[Double] = covM_gXgY(g, mu, BDV.zeros[Double](mu.size), M, (0 to 8).toList)
+    val covM00: BDM[Double] = covM_gXgY(g, BDV.zeros[Double](mu.size), BDV.zeros[Double](mu.size), M, (0 to 8).toList)
+    val M_out = pp * covM + pq * covM0 + pq.t * CovM0.t + qq * covM00
+    diag(M_out) := varT_mix(g, mu, pi) // R code did this with sapply, so the arguments to varT_mix as a scalar function
+    return BDM.zeros[Double](1,1)
+  }
+
+  /**
+    * Calculate Var[T_i], where T_i=g(Z_i), Z_i = Z_0i + mu_i*C_i, C_i ~ Bern(pi_i)
+    *
+    * @param g
+    * @param mu
+    * @param pi
+    */
+  def varT_mix(g: (Double) => Double, mu: BDV[Double], pi: BDV[Double]): BDV[Double] = {
+    return pi *:* E_gX_p(g, mu, 2.0) + (1-pi) *:* E_gX_p(g, 0.0, 2.0)  - (E_T_mix(g, mu, pi))^:^2.0
+  }
+
+
+  /**
+    * Calculate E[T_i], where T_i=g(Z_i), Z_i = Z_0i + mu_i*C_i, C_i ~ Bern(pi_i)
+    *
+    * @param g
+    * @param mu
+    * @param pi
+    */
+  def E_T_mix(g: (Double) => Double, mu: BDV[Double], pi: BDV[Double]): BDV[Double] = {
+    return pi * E_gX_p(g, mu, 1) + (1.0 - pi) * E_gX_p(g, 0.0, 1)
   }
 
 }
